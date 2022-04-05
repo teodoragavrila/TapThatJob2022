de42f8842cedaac522c85c62259e223dde6ff8ad
Catalin Comanescu <catalin@comanescu.dev>
2022-04-05 16:56:53 +0300

    Extracting params - sort games

--- react-router-demo/src/components/GameList.js
+++ react-router-demo/src/components/GameList.js
@@ -1,18 +1,49 @@
+import { useLocation, useNavigate } from 'react-router-dom';
 import GameItem from './GameItem';
 import classes from './GameList.module.css';
 
+const sortGames = (games, ascending) => {
+  return games.sort((gameA, gameB) => {
+    if (ascending) {
+      return gameA.name > gameB.name ? 1 : -1;
+    } else {
+      return gameA.name < gameB.name ? 1 : -1;
+    }
+  });
+};
+
 const GameList = (props) => {
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const queryParams = new URLSearchParams(location.search);
+
+  const isSortingAscending = queryParams.get('sort') === 'asc';
+
+  const sortedGames = sortGames(props.games, isSortingAscending);
+
+  const changeSortingHandler = () => {
+    navigate(`${location.pathname}?sort=${(isSortingAscending ? 'desc' : 'asc')}`);
+  };
+
   return (
-    <ul className={classes.list}>
-      {props.games.map((game) => (
-        <GameItem
-          key={game.id}
-          id={game.id}
-          name={game.name}
-          details={game.details}
-        />
-      ))}
-    </ul>
+    <>
+      <div className={classes.sorting}>
+        <button onClick={changeSortingHandler}>
+          Sort {isSortingAscending ? 'Descending' : 'Ascending'}
+        </button>
+      </div>
+      <ul className={classes.list}>
+        {sortedGames.map((game) => (
+          <GameItem
+            key={game.id}
+            id={game.id}
+            name={game.name}
+            details={game.details}
+          />
+        ))}
+      </ul>
+    </>
   );
 };
 
